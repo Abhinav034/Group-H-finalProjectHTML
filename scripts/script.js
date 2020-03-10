a82fd5c985cd28f58204c26489b5d27c865a6dfb
Harman Kaur <kaurharmanpreet502@gmail.com>
2020-03-10 19:07:53 -0400

    added hotel-search page on tornto hotels image link

---
+++ scripts/script.js
@@ -0,0 +1,6 @@
+
+
+function showSignUp(){
+  document.getElementsByClassName('login-form')[0].style.display = "none";
+  document.getElementsByClassName('registration-form')[0].style.display = "block";
+}
