4af88797f145b57c6b6c704ee36ae41faaf61894
Parth Dalwadi <pbdalwadi@gmail.com>
2020-03-16 20:33:00 -0400

    mylist and packagebooking developed - database integrated to all the pages. -user login and registeration data stored in database - user request data stored in my list page

--- scripts/script.js
+++ scripts/script.js
@@ -1,21 +1,13 @@
 
 
 function showSignUp(){
+
+
+
+  document.getElementById("username").required = false;
+  document.getElementById("password").required = false;
   document.getElementsByClassName('login-form')[0].style.display = "none";
+  document.getElementsByClassName('login-form')[1].style.display = "none";
   document.getElementsByClassName('registration-form')[0].style.display = "block";
+  document.getElementsByClassName('registration-form')[1].style.display = "block";
 }
-
-document.getElementById('btn-one-way').addEventListener('click', function(){
-  let departDate = document.getElementsByClassName('depart-date')[0];
-  departDate.style.width = "100%";
-  let returnDate = document.getElementsByClassName('return-date')[0];
-  returnDate.style.display = "none";
-
-});
-document.getElementById('btn-round-trip').addEventListener('click', function(){
-  let departDate = document.getElementsByClassName('depart-date')[0];
-  departDate.style.width = "47%";
-  let returnDate = document.getElementsByClassName('return-date')[0];
-  returnDate.style.display = "inline-block";
-
-});
